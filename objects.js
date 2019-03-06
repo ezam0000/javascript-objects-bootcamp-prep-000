a8c087ee73a899171ce9fbcf753ba7bca776a827
esteban zambrano <ezam0000@gmail.com>
2019-03-06 22:55:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,8 @@
 var playlist=
-{'artistName':"songTitle"}
+{'nirvana':"smells"
+  'green day':"american"
+}
+
 
 function updatePlaylist(playlist,songTitle,artistName){
   return Object.assign ({playlist,artistName,songTitle})
