fc911ecdaf082c8e567d5ae03588fa0268ae52f4
esteban zambrano <ezam0000@gmail.com>
2019-03-06 23:08:12 +0000

    Automatically backed up by Learn

--- objects.js
+++
@@ -1,8 +0,0 @@
-var Slowdive = {
-  'My Bloody Valentine':'Phil Ochs',
-  greenday:'american'
-}
-
-function updatePlaylist(Slowdive,my Bloody Valentine, Phil Ochs){
-  return Object.assign ({},playlist,{[nirvana]:smells})
-}
